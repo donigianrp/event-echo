53172e7e2d9c225d401142e23b3bcef824fcdc22
matthewclunie <matthew.clunie@hunterbusinessschool.edu>
2023-12-29 11:15:46 -0500

    timeline page outline, genera functionality

--- app/event_series/timeline/page.tsx
+++ app/event_series/timeline/page.tsx
@@ -1,81 +1,176 @@
+'use client';
+
 import { Button } from '@/app/components/ui/button';
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/app/components/ui/card';
-import { Input } from '@/app/components/ui/input';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import {
+  BarChart4,
+  Cloud,
+  LineChart,
+  Pause,
+  PieChart,
+  Play,
+} from 'lucide-react';
+import { Slider } from '@/components/ui/slider';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-const timeline = () => {
+const data = [
+  { eventName: 'Video 1', wordCount: 47 },
+  { eventName: 'Video 2', wordCount: 22 },
+  { eventName: 'Video 3', wordCount: 2 },
+  { eventName: 'Video 4', wordCount: 4 },
+  { eventName: 'Video 5', wordCount: 1 },
+];
+
+const Timeline = () => {
+  const [value, setValue] = useState([0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    let handlePlay: NodeJS.Timeout | undefined;
+
+    if (isPlaying) {
+      const increment = () => {
+        let num = ++value[0];
+        if (value[0] >= data.length - 1) {
+          setIsPlaying(false);
+          clearInterval(handlePlay);
+        }
+        setValue([num]);
+      };
+      handlePlay = setInterval(increment, 1000);
+    }
+    return () => clearInterval(handlePlay);
+  }, [isPlaying]);
+
+  const handleChange = (e: number[]) => {
+    setValue([e[0]]);
+    setIsPlaying(false);
+  };
+
+  const pause = () => {
+    setIsPlaying(false);
+  };
+
+  const play = () => {
+    if (value[0] !== data.length - 1) {
+      setIsPlaying(true);
+    } else {
+      setValue([0]);
+      setIsPlaying(true);
+    }
+  };
+
   return (
-    <div className="flex justify-center pb-4">
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <div className="flex justify-center p-2 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Timeline
-        </div>
-        <div className="flex">
-          <Tabs defaultValue="account" className="w-[800px]">
-            <TabsList className="grid w-full grid-cols-3">
-              <TabsTrigger value="bar">Bar Chart</TabsTrigger>
-              <TabsTrigger value="line">Line Chart</TabsTrigger>
-              <TabsTrigger value="pie">Pie Chart</TabsTrigger>
+    <div>
+      <div className="flex justify-center p-2 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Timeline
+      </div>
+      <div>
+        <div className="px-40">
+          <div className="flex justify-end my-2">
+            <Slider
+              defaultValue={[0]}
+              value={value}
+              max={data.length - 1}
+              step={1}
+              className="mr-4"
+              onValueChange={(e) => handleChange(e)}
+            />
+            {isPlaying ? (
+              <Button
+                variant="outline"
+                size="icon"
+                onClick={() => {
+                  pause();
+                }}
+              >
+                <Pause className="h-4 w-4" />
+              </Button>
+            ) : (
+              <Button
+                variant="outline"
+                size="icon"
+                onClick={() => {
+                  play();
+                }}
+              >
+                <Play className="h-4 w-4" />
+              </Button>
+            )}
+          </div>
+          <Tabs defaultValue="bar">
+            <TabsList className="grid w-full grid-cols-4">
+              <TabsTrigger value="bar">
+                <BarChart4 className="h-5" />
+              </TabsTrigger>
+              <TabsTrigger value="line">
+                <LineChart className="h-5" />
+              </TabsTrigger>
+              <TabsTrigger value="pie">
+                <PieChart className="h-5" />
+              </TabsTrigger>
+              <TabsTrigger value="cloud">
+                <Cloud className="h-5" />
+              </TabsTrigger>
             </TabsList>
             <TabsContent value="bar">
               <Card>
                 <CardHeader>
-                  <CardTitle>Bar Chart</CardTitle>
-                  <CardDescription>(Event Series Name)</CardDescription>
+                  <CardTitle>Event Series Name</CardTitle>
+                  <CardDescription>{data[value[0]].eventName}</CardDescription>
                 </CardHeader>
-                <CardContent className="space-y-8">
-                  <div className="space-y-1">
-                    <Input id="name" defaultValue="Pedro Duarte" />
-                  </div>
-                  <div className="space-y-1">
-                    <Input id="username" defaultValue="@peduarte" />
+                <CardContent className="space-y-2">
+                  <div className="flex justify-center items-center h-80">
+                    {data[value[0]].wordCount}
                   </div>
                 </CardContent>
-                <CardFooter>(Video/Event Name)</CardFooter>
               </Card>
             </TabsContent>
             <TabsContent value="line">
               <Card>
                 <CardHeader>
-                  <CardTitle>Line Chart</CardTitle>
-                  <CardDescription>
-                    Change your password here. After saving, youll be logged
-                    out.
-                  </CardDescription>
+                  <CardTitle>Event Series Name</CardTitle>
+                  <CardDescription>{data[value[0]].eventName}</CardDescription>
                 </CardHeader>
                 <CardContent className="space-y-2">
-                  <div className="space-y-1">
-                    <Input id="current" type="password" />
-                  </div>
-                  <div className="space-y-1">
-                    <Input id="new" type="password" />
+                  <div className="flex justify-center items-center h-80">
+                    {data[value[0]].wordCount}
                   </div>
                 </CardContent>
-                <CardFooter>(Event Series Name)</CardFooter>
               </Card>
             </TabsContent>
             <TabsContent value="pie">
               <Card>
                 <CardHeader>
-                  <CardTitle>Line Chart</CardTitle>
-                  <CardDescription>A third thing.</CardDescription>
+                  <CardTitle>Event Series Name</CardTitle>
+                  <CardDescription>{data[value[0]].eventName}</CardDescription>
                 </CardHeader>
                 <CardContent className="space-y-2">
-                  <div className="space-y-1">
-                    <Input id="current" type="password" />
-                  </div>
-                  <div className="space-y-1">
-                    <Input id="new" type="password" />
+                  <div className="flex justify-center items-center h-80">
+                    {data[value[0]].wordCount}
                   </div>
                 </CardContent>
-                <CardFooter>(Event Series Name)</CardFooter>
+              </Card>
+            </TabsContent>
+            <TabsContent value="cloud">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Event Series Name</CardTitle>
+                  <CardDescription>{data[value[0]].eventName}</CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div className="flex justify-center items-center h-80">
+                    {data[value[0]].wordCount}
+                  </div>
+                </CardContent>
               </Card>
             </TabsContent>
           </Tabs>
@@ -85,4 +180,4 @@ const timeline = () => {
   );
 };
 
-export default timeline;
+export default Timeline;
