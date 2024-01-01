23d2f4b43386b07af2c8f7c8a6a5510627a02e72
RichDoherty <doherty622@gmail.com>
2024-01-01 15:45:56 -0500

    basic login page layout

--- app/login/components/sign_in.tsx
+++ app/login/components/sign_in.tsx
@@ -5,7 +5,10 @@ import { signIn } from 'next-auth/react';
 
 export default function SignIn() {
   return (
-    <Button onClick={() => signIn('google', { callbackUrl: '/' })}>
+    <Button
+      variant={'outline'}
+      onClick={() => signIn('google', { callbackUrl: '/' })}
+    >
       Sign in with Google
     </Button>
   );
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,25 +1,45 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]/route';
 import SignIn from './components/sign_in';
+import { Input } from '../components/ui/input';
+import { Button } from '../components/ui/button';
+import { Separator } from '../components/ui/separator';
+import Link from 'next/link';
 
 const Login = async () => {
   const session = await getServerSession(authOptions);
 
   return (
-    <div className="w-1/2 mx-auto">
-      {session && (
-        <div>
-          <p>Signed in as {session.user && session.user.name}</p>
-          <a href="/api/auth/signout">Sign out by link</a>
+    <div className="grid grid-cols-2 h-screen">
+      <div className="flex flex-col bg-slate-700 bg-opacity-30 backdrop-blur-md">
+        <h1 className="p-8 font-medium text-2xl">Event Echo</h1>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="flex flex-col mx-auto p-8 space-y-6 w-full sm:w-3/5">
+          <div className="flex flex-col space-y-2 text-center">
+            <h2 className="text-2xl font-semibold tracking-tight">Sign In</h2>
+            <p className="text-sm text-muted-foreground">
+              Enter your email to log in
+            </p>
+          </div>
+          <div className="grid gap-6">
+            <form>
+              <div className="grid gap-2">
+                <Input id="email" type="email" placeholder="name@example.com" />
+                <Button type="submit">Sign In</Button>
+              </div>
+            </form>
+            <Separator />
+            <SignIn />
+            <div className="text-center text-muted-foreground">
+              {`Don't have an account? `}
+              <Link href={'/'} className="underline hover:text-primary">
+                Sign up
+              </Link>{' '}
+            </div>
+          </div>
         </div>
-      )}
-
-      {!session && (
-        <div>
-          <p>Not signed in</p>
-          <SignIn />
-        </div>
-      )}
+      </div>
     </div>
   );
 };
