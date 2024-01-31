fd2f8bd62dc0fe15cc3a6bba46825ecfc87709d4
RichDoherty <doherty622@gmail.com>
2024-01-31 11:04:48 -0500

    add check to user edit page

--- app/user/[id]/edit/page.tsx
+++ app/user/[id]/edit/page.tsx
@@ -1,8 +1,18 @@
 import prisma from '@/db';
 import EditUserForm from '../../conponents/edit_user_form';
+import { getServerSession } from 'next-auth';
+import LoginPrompt from '@/app/components/login_prompt';
 
 export default async function EditUser({ params }: { params: { id: string } }) {
+  const session = await getServerSession();
+  if (!session) {
+    return <p>Not logged in.</p>;
+  }
+
   const id = Number(params.id);
+  if (session.user.id !== id) {
+    return <p>Cannot edit another user&#39;s profile.</p>;
+  }
   const user = await prisma.user.findUnique({
     where: {
       id: id,
