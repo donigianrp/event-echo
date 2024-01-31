3b41928507a053822f57bc526d7af60afe2d113f
matthewclunie <matthew.clunie@hunterbusinessschool.edu>
2024-01-31 16:38:07 -0500

    added favorites conditional

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import { getServerSession } from 'next-auth';
+import React from 'react';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import LoginPrompt from '../components/login_prompt';
 
-const Favorites = () => {
+const Favorites = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return <LoginPrompt />;
+  }
+
   return <div className="flex justify-center p-2">Favorites</div>;
 };
 
