03604cf8ab6644850a3d9c6881fa98ee80386b7e
matthewclunie <matthew.clunie@hunterbusinessschool.edu>
2024-02-23 11:05:42 -0500

    removed second main tag

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Feed from './components/feed';
 
 export default function Home() {
   return (
-    <main>
+    <>
       <div className="flex flex-col justify-center p-2">
         Event Echo - Home of Your Voice
       </div>
       <Feed />
-    </main>
+    </>
   );
 }
