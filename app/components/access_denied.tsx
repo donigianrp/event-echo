8bc2afe62f2141a37d9a309aaf1cd12c57f30d3d
RichDoherty <doherty622@gmail.com>
2024-02-27 10:21:17 -0500

    add back button to access denied component

--- app/components/access_denied.tsx
+++ app/components/access_denied.tsx
@@ -1,5 +1,9 @@
-import { Lock } from 'lucide-react';
+'use client';
+
+import { ChevronLeft, Lock } from 'lucide-react';
 import LoginPrompt from './login_prompt';
+import { useRouter } from 'next/navigation';
+import { Button } from './ui/button';
 
 export default function AccessDenied({
   message,
@@ -8,6 +12,8 @@ export default function AccessDenied({
   message: string;
   loggedIn: boolean;
 }) {
+  const router = useRouter();
+
   return (
     <div className="flex h-screen items-center">
       <div className="flex flex-col gap-4 mx-auto text-center">
@@ -15,6 +21,14 @@ export default function AccessDenied({
         <h1 className="text-3xl">Access denied</h1>
         <p className="text-lg">{message}</p>
         {!loggedIn && <LoginPrompt />}
+        <Button
+          variant="outline"
+          className="mx-auto size-fit pl-2"
+          onClick={router.back}
+        >
+          <ChevronLeft size={20} />
+          Back
+        </Button>
       </div>
     </div>
   );
