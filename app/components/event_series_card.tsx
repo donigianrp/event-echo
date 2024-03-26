0449981f94db439192569fb550a9f34bd071acb8
matthewclunie <matthew.clunie@hunterbusinessschool.edu>
2024-03-26 19:53:36 -0400

    event series card update

--- app/components/event_series_card.tsx
+++ app/components/event_series_card.tsx
@@ -1,17 +1,11 @@
 'use client';
 
-import Link from 'next/link';
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-  CardContent,
-} from './ui/card';
-import React from 'react';
-import { Thumbnails } from '../workshop/[id]/page';
 import Image from 'next/image';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+import { Thumbnails } from '../workshop/[id]/page';
+import { Card, CardContent, CardDescription, CardTitle } from './ui/card';
 
 const EventSeriesCard = (props: {
   id: number;
@@ -27,46 +21,34 @@ const EventSeriesCard = (props: {
     link = `/event_series/${props.id}`;
   }
 
+  const [imageError, setImageError] = useState(false);
+
   return (
-    <Card className="mx-auto w-full">
-      <Link href={link}>
-        <CardHeader>
-          <div className="flex justify-between items-center">
-            {props.thumbnails && (
-              <div className="mr-4 hidden sm:inline-block">
-                <Image
-                  className="rounded-sm"
-                  alt="video thumbnail"
-                  src={props.thumbnails.medium.url}
-                  height={props.thumbnails.medium.height}
-                  width={props.thumbnails.medium.width}
-                />
-              </div>
-            )}
-            <div className="w-full">
-              <CardTitle className="overflow-hidden line-clamp-2 w-full leading-tight">
-                {props.title}
-              </CardTitle>
-              {props.description && (
-                <CardDescription className="h-10 overflow-hidden line-clamp-2 mt-2">
-                  {props.description}
-                </CardDescription>
-              )}
-            </div>
-          </div>
-        </CardHeader>
-        {props.thumbnails && (
-          <CardContent className="flex justify-center items-center sm:hidden">
+    <Card>
+      <CardContent className="flex mx-auto w-full h-40 items-center overflow-hidden relative p-4 justify-center">
+        <Link href={link}>
+          {props.thumbnails && !imageError && (
             <Image
-              className="rounded-sm w-2/3 inline-block sm:hidden"
+              className="rounded-sm absolute top-0 left-0 w-full object-fill mix-blend-darken dark:mix-blend-lighten blur-sm transition-all duration-500 opacity-25 hover:opacity-5 hover:scale-125"
               alt="video thumbnail"
               src={props.thumbnails.medium.url}
               height={props.thumbnails.medium.height}
               width={props.thumbnails.medium.width}
+              onError={(e) => setImageError(true)}
             />
-          </CardContent>
-        )}
-      </Link>
+          )}
+          <div className="w-full">
+            <CardTitle className="overflow-hidden line-clamp-2 w-full leading-tight">
+              {props.title}
+            </CardTitle>
+            {props.description && (
+              <CardDescription className="h-10 overflow-hidden line-clamp-2 mt-2">
+                {props.description}
+              </CardDescription>
+            )}
+          </div>
+        </Link>
+      </CardContent>
     </Card>
   );
 };
